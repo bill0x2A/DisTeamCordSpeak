7a32e4e6581411f8863d1b5dbb76a51650733770
bill0x2A <billysmith1998@live.com>
2021-01-17 16:52:59 +0000

    Add bot to home channel, remove unmute sound

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.channels.fetch('766823683376676888')
+    .then(channel => {
+      channel.join();
+    })
 });
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
   if (oldState.channel !== newState.channel) {
-    if (oldState.channel && oldState.channel.name === 'General') {
+    if (oldState.channel && oldState.channel.name === 'Home') {
       play(oldState.channel, './sounds/disconnected.wav');
     }
 
-    if (newState.channel && newState.channel.name === 'General') {
-      const havingAGoodTime = Math.random() <= 0.05;
+    if (newState.channel && newState.channel.name === 'Home') {
+      const havingAGoodTime = Math.random() <= 0.01;
       const file = havingAGoodTime ? './sounds/connected-but-its-loud-af.mp3' : './sounds/connected.wav';
       play(newState.channel, file);
     }
   }
 
-  if (oldState.selfDeaf && !newState.selfDeaf) {
-    play(oldState.channel, './sounds/talkpower_granted.wav');
-  }
 })
 
 client.login(process.env.DISCORD_BOT_TOKEN);
